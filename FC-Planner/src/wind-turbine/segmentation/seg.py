16e1dded88967262709b3126640bdada57d288d6
KasperMollerHansen <kasperhansen1998@hotmail.com>
2024-11-08 11:01:45 +0100

    load_wind

--- FC-Planner/src/wind-turbine/segmentation/seg.py
+++ FC-Planner/src/wind-turbine/segmentation/seg.py
@@ -1,12 +1,32 @@
 #%%
 import open3d as o3d
 import os
-
 # %%
-print()
+class Segmentation:
+    FILE_PATH = os.path.abspath(__file__)
 
-#pcd = o3d.io.read_point_cloud("../../wind-turbine/data/windmill.pcd")
-# Visualize the point cloud
-#o3d.visualization.draw_geometries([pcd])
+    def __init__(self):
+        pass
 
+    def _get_path_to_src(self):
+        path = self.FILE_PATH
+        while not path.endswith("src"):
+            path = os.path.dirname(path)
+        return path
+    
+    def _read_point_cloud(self, path, file_name):
+        pcd = o3d.io.read_point_cloud(path + file_name)
+        return pcd
+    
+    def main(self):
+        src_path = self._get_path_to_src()
+        pcd = self._read_point_cloud(src_path + "/wind-turbine/data/", "windmill.pcd")
+        o3d.visualization.draw_geometries([pcd])
+
+
+#%%
+if __name__ == "__main__":
+    seg = Segmentation()
+    seg.main()
 # %%
+
