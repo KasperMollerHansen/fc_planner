316a7f381b2ade577e6733a2e5231aacbe2f976c
KasperMollerHansen <kasperhansen1998@hotmail.com>
2024-11-08 10:23:35 +0100

    seg

--- FC-Planner/src/wind-turbine/segmentation/seg.py
+++ FC-Planner/src/wind-turbine/segmentation/seg.py
@@ -1,3 +1,12 @@
 #%%
 import open3d as o3d
+import os
 
+# %%
+print()
+
+#pcd = o3d.io.read_point_cloud("../../wind-turbine/data/windmill.pcd")
+# Visualize the point cloud
+#o3d.visualization.draw_geometries([pcd])
+
+# %%
