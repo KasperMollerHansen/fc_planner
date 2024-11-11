e0b0fff0fc478a4900c3632ec5c0184e0bd4c334
BjarkeHJ <bjarke_holm@hotmail.com>
2024-11-11 17:42:34 +0100

    -

--- FC-Planner/src/hierarchical_coverage_planner/src/hcplanner.cpp
+++ FC-Planner/src/hierarchical_coverage_planner/src/hcplanner.cpp
@@ -597,7 +597,7 @@ namespace predrecon
       vector<int>().swap(segments_id);
       segments_id = skeleton_operator->P.branch_seg_pairs[sub_id];
       
-      cout << "Segment ID" << segments_id << '\n';
+      // cout << "Segment ID" << segments_id << '\n';
 
       // Sliced vector
       vector<int> slice(segments_id.begin() + 5, segments_id.end());
