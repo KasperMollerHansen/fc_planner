2a2fbc3c1273209089590891884260bd61f926b4
KasperMollerHansen <kasperhansen1998@hotmail.com>
2024-11-12 17:30:29 +0100

    sys_path

--- FC-Planner/src/wind-turbine/segmentation/segmentation.py
+++ FC-Planner/src/wind-turbine/segmentation/segmentation.py
@@ -1,4 +1,8 @@
 #%%
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
 import open3d as o3d
 import numpy as np
 import functions as f
